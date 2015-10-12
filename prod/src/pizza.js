56123d5d35546c1407ce3a19a2aa072185f233b6
adiherzog <adrian.herzog@gmail.com>
2015-10-12 11:08:10 +0200

    Added a phone number and an address step to support multiple scenarios.

--- prod/src/pizza.js
+++ prod/src/pizza.js
@@ -1,57 +1,96 @@
 (function() {
 
-    var pizzaNextButton = $('#pizza_next');
-    var drinkNextButton = $('#drink_next');
-    var summaryNextButton = $('#summary_next');
-    var stepOne = $('#step1');
-    var stepTwo = $('#step2');
-    var stepThree = $('#step3');
-    var stepFour = $('#step4');
+    // Wizzard steps
+    var steps = {
+        enterPhoneNumber : $('#step-enterPhoneNumber'),
+        confirmAddress : $('#step-confirmAddress'),
+        enterAddress : $('#step-enterAddress'),
+        selectPizza : $('#step-selectPizza'),
+        selectDrinks : $('#step-selectDrinks'),
+        summary : $('#step-summary'),
+        confirmation : $('#step-confirmation')
+    };
 
+    // UI state
+    var currentStep = steps.enterPhoneNumber;
+
+    // Collected data
+    var phoneNumber = '';
     var selectedPizza = '';
     var selectedDrinks = [];
 
-    pizzaNextButton.click(function() {
+    steps.enterPhoneNumber.find('.next').click(function() {
+        var KNOWN_NUMBER = '0791111111';
+
+        phoneNumber = $('#phoneNumber').val();
+
+        if(phoneNumber.trim() === '') {
+            alert('Please enter a phone number');
+            return;
+        }
+
+        if(phoneNumber === KNOWN_NUMBER) {
+            goToStep(steps.confirmAddress);
+        } else {
+            goToStep(steps.enterAddress);
+        }
+    });
+
+    steps.confirmAddress.find('.yes').click(function() {
+        goToStep(steps.selectPizza);
+    });
+
+    steps.confirmAddress.find('.no').click(function() {
+        goToStep(steps.enterAddress);
+    });
+
+    steps.enterAddress.find('.next').click(function() {
+        goToStep(steps.selectPizza);
+    });
+
+    steps.selectPizza.find('.next').click(function() {
         selectedPizza = $("input:radio[name ='pizza']:checked").val();
-        console.log(selectedPizza);
         if(typeof selectedPizza === 'undefined') {
             alert("Please select a pizza!");
             return;
         }
-        stepOne.hide();
-        stepTwo.show();
+        goToStep(steps.selectDrinks);
     });
 
-    drinkNextButton.click(function() {
-        stepTwo.hide();
-        stepThree.show();
+    steps.selectDrinks.find('.next').click(function() {
         rememberSelectedDrinks();
         addPizzaAndDrinkToSummary();
-    });
+        goToStep(steps.summary);
 
-    function rememberSelectedDrinks() {
-        selectedDrinks = [];
-        stepTwo.find('input').each(function (index) {
-            var element = $(this);
-            if (element.is(':checked')) {
-                selectedDrinks.push(element.val());
+        function rememberSelectedDrinks() {
+            selectedDrinks = [];
+            steps.selectDrinks.find('input').each(function (index) {
+                var element = $(this);
+                if (element.is(':checked')) {
+                    selectedDrinks.push(element.val());
+                }
+            });
+            if(selectedDrinks.length === 0) {
+                selectedDrinks.push('none');
             }
-        });
-        if(selectedDrinks.length === 0) {
-            selectedDrinks.push('none');
         }
-    }
 
-    function addPizzaAndDrinkToSummary() {
-        $('#summary_pizza').text(selectedPizza);
-        $('#summary_drinks').text(selectedDrinks.join(", "));
-        $('#confirmation_pizza').text(selectedPizza);
-        $('#confirmation_drinks').text(selectedDrinks.join(", "));
-    }
-
-    summaryNextButton.click(function() {
-        stepThree.hide();
-        stepFour.show();
+        function addPizzaAndDrinkToSummary() {
+            $('#summary_pizza').text(selectedPizza);
+            $('#summary_drinks').text(selectedDrinks.join(", "));
+            $('#confirmation_pizza').text(selectedPizza);
+            $('#confirmation_drinks').text(selectedDrinks.join(", "));
+        }
     });
+
+    steps.summary.find('.next').click(function() {
+       goToStep(steps.confirmation);
+    });
+
+    function goToStep(toStep) {
+        currentStep.hide();
+        toStep.show();
+        currentStep = toStep;
+    }
 
 })();
