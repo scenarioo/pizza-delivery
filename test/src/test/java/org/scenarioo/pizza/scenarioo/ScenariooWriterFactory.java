cd52eb9fc8c7e3cf3a24d0a19510f839042be06c
Ivo de Concini <ivo.deconcini@zuehlke.com>
2017-10-05 09:30:35 +0200

    Get build name from env variable

--- test/src/test/java/org/scenarioo/pizza/scenarioo/ScenariooWriterFactory.java
+++ test/src/test/java/org/scenarioo/pizza/scenarioo/ScenariooWriterFactory.java
@@ -2,12 +2,9 @@ package org.scenarioo.pizza.scenarioo;
 
 import org.scenarioo.api.ScenarioDocuWriter;
 
-import java.io.File;
-
 public class ScenariooWriterFactory {
 
     public static ScenarioDocuWriter getNewWriter() {
-        TestConstants.DOCU_FOLDER.mkdirs();
         return new ScenarioDocuWriter(TestConstants.DOCU_FOLDER, TestConstants.BRANCH, TestConstants.BUILD);
     }
 
--- test/src/test/java/org/scenarioo/pizza/scenarioo/TestConstants.java
+++ test/src/test/java/org/scenarioo/pizza/scenarioo/TestConstants.java
@@ -8,7 +8,12 @@ public class TestConstants {
 
     public static final String BRANCH = "gh-pages";
     public static final LocalDateTime DATE = LocalDateTime.now();
-    public static final String BUILD = DATE.format(DateTimeFormatter.ISO_DATE_TIME);
-    public static final File DOCU_FOLDER = new File("build/scenariooDocumentation");
+    public static final String BUILD = System.getenv("BUILD_NUMBER");
+    public static final File DOCU_FOLDER;
+
+    static {
+        DOCU_FOLDER = new File("build/scenariooDocumentation");
+        DOCU_FOLDER.mkdirs(); // ensure folder exists
+    }
 
 }
