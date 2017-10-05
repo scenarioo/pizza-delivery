dbc6c6cae22189f07f1195c1607995be42e03fd5
Ivo de Concini <ivo.deconcini@zuehlke.com>
2017-10-05 11:03:23 +0200

    env

--- test/src/test/java/org/scenarioo/pizza/scenarioo/TestConstants.java
+++ test/src/test/java/org/scenarioo/pizza/scenarioo/TestConstants.java
@@ -3,17 +3,22 @@ package org.scenarioo.pizza.scenarioo;
 import java.io.File;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 public class TestConstants {
 
-    public static final String BRANCH = System.getenv("BRANCH_NAME");
+    public static final String BRANCH = getEnvOrDefault("BRANCH_NAME", "gh-pages");
     public static final LocalDateTime DATE = LocalDateTime.now();
-    public static final String BUILD = System.getenv("BUILD_NUMBER");
+    public static final String BUILD = getEnvOrDefault("BUILD_NAME", DATE.format(DateTimeFormatter.ISO_DATE_TIME));
     public static final File DOCU_FOLDER;
 
     static {
         DOCU_FOLDER = new File("build/scenariooDocumentation");
         DOCU_FOLDER.mkdirs(); // ensure folder exists
+    }
+
+    private static String getEnvOrDefault(String name, String defaultValue) {
+        return Optional.ofNullable(System.getenv(name)).orElse(defaultValue);
     }
 
 }
