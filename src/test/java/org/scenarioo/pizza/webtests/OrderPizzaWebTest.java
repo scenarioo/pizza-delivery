89d60afc2e8efbd44f30dc807262dbafdf3c87b8
Rolf Bruderer <bruderer@computerscience.ch>
2020-01-26 21:22:23 +0100

    Improve test naming in examples

--- src/test/java/org/scenarioo/pizza/webtests/OrderPizzaWebTest.java
+++ src/test/java/org/scenarioo/pizza/webtests/OrderPizzaWebTest.java
@@ -12,7 +12,7 @@ public class OrderPizzaWebTest extends WebTest {
     @Labels({"main flow"})
     @Description("Typical order process for a customer with a phone number " +
             "that is already registered with the correct address.")
-    public void orderPizza_plusRedWine() {
+    public void existingCustomer_canOrderPizzaOnRegisteredAddress() {
         EnterPhoneNumberPage.navigateToPage();
         EnterPhoneNumberPage.enterKnownPhoneNumber();
         EnterPhoneNumberPage.clickNext();
@@ -35,7 +35,7 @@ public class OrderPizzaWebTest extends WebTest {
     @Test
     @Labels({"partial flow", "enter address"})
     @Description("Phone number is already known, but the customer wants to correct the address.")
-    public void knownPhoneNumber_butIncorrectPhoneNumber() {
+    public void existingCustomer_canOrderPizzaOnDifferentAddress() {
         EnterPhoneNumberPage.navigateToPage();
         EnterPhoneNumberPage.enterKnownPhoneNumber();
         EnterPhoneNumberPage.clickNext();
@@ -53,7 +53,7 @@ public class OrderPizzaWebTest extends WebTest {
     @Test
     @Labels({"partial flow", "enter address"})
     @Description("Phone number is not known yet, therefore the customer has to enter the address.")
-    public void unknownPhoneNumber_asksToEnterAddress() {
+    public void newCustomer_canRegisterWithAddressToOrderPizza() {
         EnterPhoneNumberPage.navigateToPage();
         EnterPhoneNumberPage.enterUnknownPhoneNumber();
         EnterPhoneNumberPage.clickNext();
