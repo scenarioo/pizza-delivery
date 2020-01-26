4fa41e3e60ba87897e1f1040654650edd216e342
Rolf Bruderer <bruderer@computerscience.ch>
2020-01-26 21:18:46 +0100

    Move pizza web app to docs folder for easier management of deployments on github pages through master branch merges.

---
+++ docs/README.md
@@ -0,0 +1,9 @@
+# Pizza Delivery Web Shop
+
+This docs folder simply contains the pizza web shop.
+
+This is helpful to automatically deploy the pizza shop on github pages from master branch.
+
+You can therefore browse the master version of the shop under following URL and use it for demo test projects:
+
+http://scenarioo.github.io/pizza-delivery/pizza-shop
---
+++ docs/pizza-shop/src/pizza.js
@@ -0,0 +1,125 @@
+(function () {
+
+    // Wizard steps
+    var steps = {
+        enterPhoneNumber: $('#step-enterPhoneNumber'),
+        confirmAddress: $('#step-confirmAddress'),
+        enterAddress: $('#step-enterAddress'),
+        selectPizza: $('#step-selectPizza'),
+        selectDrinks: $('#step-selectDrinks'),
+        summary: $('#step-summary'),
+        confirmation: $('#step-confirmation')
+    };
+
+    // UI state
+    var currentStepOnPage = $('#currentStep');
+    var currentStep;
+    setCurrentStep(steps.enterPhoneNumber);
+
+    // Collected data
+    var phoneNumber = '';
+    var selectedPizza = '';
+    var selectedDrinks = [];
+
+    steps.enterPhoneNumber.find('.next').click(function () {
+        var KNOWN_NUMBER = '0791111111';
+
+        phoneNumber = $('#phoneNumber').val();
+
+        if (phoneNumber.trim() === '') {
+            alert('Please enter a phone number');
+            return;
+        }
+
+        if (phoneNumber === KNOWN_NUMBER) {
+            goToStep(steps.confirmAddress);
+        } else {
+            goToStep(steps.enterAddress);
+        }
+    });
+
+    steps.confirmAddress.find('.yes').click(function () {
+        goToStep(steps.selectPizza);
+    });
+
+    steps.confirmAddress.find('.no').click(function () {
+        goToStep(steps.enterAddress);
+    });
+
+    steps.enterAddress.find('.next').click(function () {
+        goToStep(steps.selectPizza);
+    });
+
+    steps.selectPizza.find('.next').click(function () {
+        selectedPizza = $("input:radio[name ='pizza']:checked").val();
+        if (typeof selectedPizza === 'undefined') {
+            alert("Please select a pizza!");
+            return;
+        }
+        goToStep(steps.selectDrinks);
+    });
+
+    steps.selectDrinks.find('.next').click(function () {
+        rememberSelectedDrinks();
+        addPizzaAndDrinkToSummary();
+
+        goToStep(steps.summary);
+
+        function rememberSelectedDrinks() {
+            selectedDrinks = [];
+            steps.selectDrinks.find('input').each(function (index) {
+                var element = $(this);
+                if (element.is(':checked')) {
+                    selectedDrinks.push(element.val());
+                }
+            });
+            if (selectedDrinks.length === 0) {
+                selectedDrinks.push('none');
+            }
+        }
+
+        function addPizzaAndDrinkToSummary() {
+            $('#summary_pizza').text(selectedPizza);
+            $('#summary_drinks').text(selectedDrinks.join(", "));
+            $('#confirmation_pizza').text(selectedPizza);
+            $('#confirmation_drinks').text(selectedDrinks.join(", "));
+        }
+    });
+
+    steps.summary.find('.next').click(function () {
+        var processingOrderDiv = steps.summary.find('#processing-order');
+        var numberOfDots = 1;
+        var animation = true;
+
+        steps.summary.find('.next').hide();
+        processingOrderDiv.show();
+        animateProcessing();
+
+        setTimeout(() => {
+            animation = false;
+            goToStep(steps.confirmation);
+        }, 10 * 1000);
+
+        function animateProcessing() {
+            if (!animation) {
+                return;
+            }
+
+            processingOrderDiv.text('Processing order' + '.'.repeat(numberOfDots));
+            numberOfDots = (numberOfDots + 1) % 4;
+            setTimeout(animateProcessing, 400);
+        }
+    });
+
+    function goToStep(toStep) {
+        currentStep.hide();
+        toStep.show();
+        setCurrentStep(toStep);
+    }
+
+    function setCurrentStep(step) {
+        currentStep = step;
+        currentStepOnPage.text(step.attr('id'));
+    }
+
+})();
